cb62db92866b45c12a85a76a0aa5f5e496c62cbc
Radik Fattakhov <radikft@gmail.com>
2012-01-08 18:26:02 +0400

    question.html template updated - 'for' loop for answers

--- src/qna/views.py
+++ src/qna/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from qna.models import Question
+from qna.models import Question, Answer
 
 def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    answer_list = get_list_or_404(Answer, question = question_id)
     c = {}
     c["question"] = question
+    c["answer_list"] = answer_list
     return render(request, "qna/question.html", c)
 
 def question_list(request):
